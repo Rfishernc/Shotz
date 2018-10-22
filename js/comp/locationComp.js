c888cd53857fc48545b976c70058bfb6f0d8a1f5
RichFisher <rich.fishernc@gmail.com>
2018-10-22 14:34:49 -0500

    search filter working

--- js/comp/locationComp.js
+++ js/comp/locationComp.js
@@ -14,8 +14,8 @@ function locationsBuilder(locations) {
         domString += `<div class="card ${locations[i].shootTime}" style="width: 18rem;">`
         domString +=     `<img class="card-img-top" src="${locations[i].image}" alt="Card image cap">`
         domString +=     `<div class="card-body">`
-        domString +=         `<h5 class="card-title">${locations[i].name}</h5>`
-        domString +=         `<p class="card-text">${locations[i].address}</p>`
+        domString +=         `<h5 class="card-title locName">${locations[i].name}</h5>`
+        domString +=         `<p class="card-text locAddress">${locations[i].address}</p>`
         domString +=         `<p class="card-text">Shoot Time: ${locations[i].shootTime}</p>`
         domString +=     `</div>`
         domString += `</div>`
--- js/event.js
+++ js/event.js
@@ -2,15 +2,21 @@ import {getLocations, locationsBuilder} from './comp/locationComp.js';
 
 function searchEvent() {
     $('#input').keypress(function() {
-
+        $('.card').show();
+        let input = $('#input').val() + event.key;
+        $('.card').filter(function() {
+            if($(this).find('.locName').text().includes(input) || $(this).find('.locAddress').text().includes(input) != true ) {
+                return this;
+            }
+        }).hide();
     })
 }
 
 function timeOfDayEvent() {
     $('.timeButton').click(function() {
-        locationsBuilder(getLocations());
+        $('.card').show();
         let clicked = event.target.id;
-        $('.card').filter(function(i) {
+        $('.card').filter(function() {
              if(this.classList.contains(clicked) != true) {
                  return this;
              }
